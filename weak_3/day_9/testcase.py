cb083b1a9e8231d5f29a56e40f92411f4fb7f945
juntaepark <crossmarien@naver.com>
2019-01-04 17:49:22 +0900

    todayisFriday|Iwanttorest|please|LEAVE_ME_ALONE|SSAFY|20190104

---
+++ weak_3/day_9/testcase.py
@@ -0,0 +1,12 @@
+def second_number(*args):
+    numbers=list(args)
+    for i in numbers:
+        n=0
+        if i < numbers[n-2] and i < numbers[n-1]:
+            n+1
+        elif i > numbers[n-2] and i > numbers[n-1]:
+            n+1            
+        else:
+            return i
+        
+print (second_number(100,12,13))
