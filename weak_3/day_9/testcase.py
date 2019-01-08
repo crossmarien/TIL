4d625120dbb6e93b277330e1f95ec64b7ff9fdb0
juntaepark <crossmarien@naver.com>
2019-01-08 17:45:58 +0900

    20190108|learnedpython_function&git

--- weak_3/day_9/testcase.py
+++ weak_3/day_9/testcase.py
@@ -3,9 +3,7 @@ def second_number(*args):
     for i in numbers:
         n=0
         if i < numbers[n-2] and i < numbers[n-1]:
-            n+1
-        elif i > numbers[n-2] and i > numbers[n-1]:
-            n+1            
+            n+1           
         else:
             return i
         
---
+++ weak_4_python_function/less .bash_history= 내가 이용한 명령 기록보기.md
@@ -0,0 +1,20 @@
+less .bash_history= 내가 이용한 명령 기록보기
+
+vim = 파일 들어오기
+
+cat = 파일 보기
+
+bash= git bash 다시시작
+
+
+
+
+
+px aux: 실행중인 프로그램 보기
+
+ps aux| grep jupyter
+
+kill -9 11916
+
+
+
